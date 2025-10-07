1ad946b1da4bf13a2cc0d3fa0b3f82ad1a4d6de0
Anurag MK <iamanuragmk@gmail.com>
2025-10-07 15:58:33 +0530

    font changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+         <link
+        rel="stylesheet"
+        id="gdlr-core-google-font-css"
+        href="https://fonts.googleapis.com/css?family=Jost%3A100%2C200%2C300%2Cregular%2C500%2C600%2C700%2C800%2C900%2C100italic%2C200italic%2C300italic%2Citalic%2C500italic%2C600italic%2C700italic%2C800italic%2C900italic%7CMontserrat%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2Cregular%2Citalic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic&amp;subset=cyrillic%2Clatin%2Clatin-ext%2Ccyrillic-ext%2Cvietnamese&amp;ver=5.5.1"
+        type="text/css"
+        media="all"
+    />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
