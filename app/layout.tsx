de5c21f978d6543d23b53ee1e21d66fb5ddeb330
neethu <neethu@syinnovation.co>
2025-11-13 17:02:46 +0530

    adding foco font

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,17 +26,41 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-         <link
+      {/* Adobe Typekit Font: Foco */}
+      <link
+        rel="stylesheet"
+        href="https://use.typekit.net/hml0mge.css"
+      />
+
+      <link
         rel="stylesheet"
         id="gdlr-core-google-font-css"
-        href="https://fonts.googleapis.com/css?family=Jost%3A100%2C200%2C300%2Cregular%2C500%2C600%2C700%2C800%2C900%2C100italic%2C200italic%2C300italic%2Citalic%2C500italic%2C600italic%2C700italic%2C800italic%2C900italic%7CMontserrat%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2Cregular%2Citalic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic&amp;subset=cyrillic%2Clatin%2Clatin-ext%2Ccyrillic-ext%2Cvietnamese&amp;ver=5.5.1"
+        href="https://fonts.googleapis.com/css?family=Jost%3A100%2C200%2C300%2Cregular%2C500%2C600%2C700%2C800%2C900%2C100italic%2C200italic%2C300italic%2Citalic%2C500italic%2C600italic%2C700italic%2C800italic%2C900italic&amp;subset=cyrillic%2Clatin%2Clatin-ext%2Ccyrillic-ext%2Cvietnamese&amp;ver=5.5.1"
         type="text/css"
         media="all"
-    />
-        <link href="https://fonts.googleapis.com/css?family=Poppins:400%2C700%7CRoboto:400" rel="stylesheet" property="stylesheet" media="all" type="text/css" />
-        <link href="https://db.onlinewebfonts.com/c/9f4965a37ac189de7844a4c13c7108f5?family=LeBeauneNew" rel="stylesheet"/>
+      />
+
+     
+      <link
+        href="https://fonts.googleapis.com/css?family=Poppins:400%2C700%7CRoboto:400"
+        rel="stylesheet"
+        property="stylesheet"
+        media="all"
+        type="text/css"
+      />
+
+     
+      <link
+        href="https://db.onlinewebfonts.com/c/9f4965a37ac189de7844a4c13c7108f5?family=LeBeauneNew"
+        rel="stylesheet"
+      />
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        style={{
+          fontFamily:
+            "foco, 'LeBeauneNew', 'Jost', 'Poppins', 'Roboto', sans-serif",
+        }}
       >
         <AuthProvider>
           {children}
